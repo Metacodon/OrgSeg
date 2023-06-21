a45ab559c7d2d6409ca01e52e751fb9b1a8cd3ad
Ali <Metacodon@users.noreply.github.com>
2023-06-21 22:01:31 +0200

    document(); sample exp_info.csv added

--- R/read_exp_info.R
+++ R/read_exp_info.R
@@ -1,3 +1,13 @@
+#' Read the experiment information .csv file and extract the treatments and related indices.
+#'
+#' @param file_path Path to the experiment information .csv file.
+#'
+#' @return A list of treatments and related indices.
+#' @export
+#'
+#' @examples
+#' file_path = system.file("extdata", "exp_info.csv", package = "OrgSeg")
+#' exp_info_list <- read_exp_info(file_path)
 read_exp_info <- function(file_path) {
   info_df <- readr::read_csv(file_path)
   treatments <- info_df$Treatments
