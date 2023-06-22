2c6c4b26fb365faf73092886e300c93b03c2786b
Ali <Metacodon@users.noreply.github.com>
2023-06-22 10:29:38 +0200

    inspect_bins_table() FUN doc updated.

--- R/inspect_bins_table.R
+++ R/inspect_bins_table.R
@@ -2,7 +2,7 @@
 #'
 #' @param all_data A merged tibble of Area, Replicate, and Treatment columns for all treatments which is made by tibbleing_loop().
 #' @param breaks Specify breaks in a vector like c(0,500,1000,2000,50000) #Min/3rd Qu./Mean/3rd Qu./Max .
-#' @param tags Specify interval/bin labels in a vector like c("label1","label2","label3","label4")
+#' @param tags Specify interval/bin labels in a vector like c("label1","label2","label3","label4").
 #'
 #' @return A table to inspect bins for experiment controls.
 #' @export
